c45fc740985f78827345931a905b0105b08e2146
Devi Priyanka <dumpaladevipriya@gmail.com>
2025-03-21 14:26:53 +0530

    Update PrimeNumber.py

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,7 +1,10 @@
-def is_prime(n):
-    if n < 2: 
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
+else:
+    print("Not a prime number")
